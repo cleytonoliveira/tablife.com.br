41eecb9aaf886c3b4c419b520c9b22cf1b6e2fcc
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2023-06-11 22:25:29 -0300

    feat: implementa página home com mensagem inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Jack, o nosso amor é como um Wi-Fi: forte, rápido e me conecta o tempo todo. Você é o meu sinal de vida! 💘📶</h1>
+      <h2>#AmorSemFio</h2>
+    </div>
+  )
+}
+
+export default Home;
